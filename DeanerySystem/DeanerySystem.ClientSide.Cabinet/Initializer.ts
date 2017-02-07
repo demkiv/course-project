4783f2e7031b8be247b236aedf2d69e68dc01623
Markiyan Fostyak <markiyan.fostyak@gmail.com>
2017-02-07 22:54:30 +0200

    Integrated typescript

---
+++ DeanerySystem/DeanerySystem.ClientSide.Cabinet/Initializer.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../deanerysystem.clientside.common.utilities/querystring.ts" />
+/// <reference path="../deanerysystem.clientside.common.definitions/scripts/typings/jquery/jquery.d.ts" />
+module DeanerySystem.ClientSide {
+    import QueryString = ClientSide.Common.Utilities.QueryString;
+    export class Initializer {
+        
+        //private Color: string;
+
+        constructor(private Color: string) {
+        }
+
+        public Initialize() {
+            var url = QueryString.GetQueryString();
+            var divs = jQuery("div");
+            divs.each((index, element) => {
+                jQuery(element).css("background-color", this.Color);
+            });
+            console.log(url);
+            alert("initialzied");            
+        }
+    }
+}
+
+jQuery(document)
+    .ready(() => {
+        var initializer = new DeanerySystem.ClientSide.Initializer("gray");
+        initializer.Initialize();
+    });

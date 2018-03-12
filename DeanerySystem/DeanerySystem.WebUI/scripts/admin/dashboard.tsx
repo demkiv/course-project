0ec6dd720268df1e17ee2c6cd65a76bc28474e7b
Solomiia Demkiv <solomiia.demkiv@gmail.com>
2018-03-12 22:05:50 +0200

    Configure webpack for scripts

---
+++ DeanerySystem/DeanerySystem.WebUI/scripts/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as $ from 'jquery';
+
+debugger;
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    $('#root')[0]
+);

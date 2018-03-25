85aeec28f91bc47030fb1f5e3b0ce449e2a8595a
Markiyan Fostyak <markiyan.fostyak@gmail.com>
2018-03-26 00:16:05 +0300

    Removed unused references

--- DeanerySystem/DeanerySystem.BusinessLogic/UserImport/IImporter.cs
+++ DeanerySystem/DeanerySystem.BusinessLogic/UserImport/IImporter.cs
@@ -1,8 +1,6 @@
 ﻿using DeanerySystem.BusinessLogic.UserImport.Models;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace DeanerySystem.BusinessLogic.UserImport
 {

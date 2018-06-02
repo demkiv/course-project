6b24a16da867793ae333a61275d7585870f6b06e
Markiyan Fostyak <markiyan.fostyak@gmail.com>
2018-06-02 23:04:14 +0300

    Added accounts page

--- DeanerySystem/DeanerySystem.WebUI/Controllers/AdminController.cs
+++ DeanerySystem/DeanerySystem.WebUI/Controllers/AdminController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeanerySystem.DataAccess.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DeanerySystem.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IUnitOfWork unitOfWork;
+        public AdminController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View("Dashboard");
+        }
+
+        public IActionResult StudentAccounts()
+        {
+            return View();
+        }
+
+        public IActionResult ProfessorAccounts()
+        {
+            var accounts = unitOfWork.ProfessorRepository.Get();    
+            ViewData["Data"] = JsonConvert.SerializeObject(accounts);
+            return View();
         }
     }
 }
